297d46606bed805c41454a58602a02984eaa6391
cartikaa <dwicartika@gmail.com>
2024-08-01 13:12:38 +0700

    script

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const slides = document.querySelectorAll('.banner-slide img');
+    let currentIndex = 0;
+
+    setInterval(() => {
+        slides.forEach((slide, index) => {
+            slide.style.display = 'none';
+        });
+
+        currentIndex = (currentIndex + 1) % slides.length;
+        slides[currentIndex].style.display = 'block';
+    }, 5000); // Ubah slide setiap 5 detik
+});
